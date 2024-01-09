38e61125d856a4acc457f608ae82a96bce3a6dbc
Adrien Roches <adrien@roc-it.tech>
2024-01-09 12:27:32 +0100

    [sfstart] 01-controller : Update HelloController

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -4,15 +4,29 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 
 class HelloController extends AbstractController
 {
-    #[Route('/hello', name: 'app_hello')]
-    public function index(): Response
+    #[Route(
+        path: '/hello/{name}',
+        name: 'app_hello',
+        methods: ['GET'],
+        requirements: [
+            'name' => '[a-zA-Z]+(-[a-zA-Z]+)?',
+        ],
+        defaults: [
+            'name' => 'Adrien',
+        ],
+    )]
+    public function index(string $name): Response
     {
-        return $this->render('hello/index.html.twig', [
-            'controller_name' => 'HelloController',
-        ]);
+        return new Response(
+            content: <<<"HTML"
+            <body>
+                Hello {$name} !
+            </body>
+            HTML
+        );
     }
 }
