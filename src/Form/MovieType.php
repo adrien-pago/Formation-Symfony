19eacc2b158bf9a20d3578a0c6aa7c51db4c3865
Adrien Roches <adrien@roc-it.tech>
2024-01-10 15:04:08 +0100

    [sf-start] 06-forms : Add route for new movie

--- src/Form/MovieType.php
+++ src/Form/MovieType.php
@@ -6,6 +6,7 @@ use App\Entity\Genre;
 use App\Entity\Movie;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -16,13 +17,16 @@ class MovieType extends AbstractType
         $builder
             ->add('slug')
             ->add('title')
-            ->add('releasedAt')
+            ->add('releasedAt', DateType::class, [
+                'widget' => 'single_text',
+            ])
             ->add('plot')
             ->add('poster')
             ->add('genres', EntityType::class, [
                 'class' => Genre::class,
-'choice_label' => 'id',
-'multiple' => true,
+                'choice_label' => 'name',
+                'multiple' => true,
+                'expanded' => true,
             ])
         ;
     }
