38a60082d7cea70eecb1b70bb428d830c9dd0b47
Adrien Roches <adrien@roc-it.tech>
2024-01-12 10:10:05 +0100

    [sf-start] 09-security : Add fixtures for users

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\PasswordHasherFactoryInterface;
+
+class UserFixtures extends Fixture
+{
+    private const USERS = [
+        [
+            'username' => 'adrien',
+            'password' => 'adrien',
+        ],
+        [
+            'username' => 'max',
+            'password' => 'max',
+        ],
+    ];
+
+    public function __construct(
+        private readonly PasswordHasherFactoryInterface $hasherFactory
+    ) {
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        foreach (self::USERS as $userDetails) {
+            $user = (new User())
+                ->setUsername($userDetails['username'])
+                ->setPassword($this->hasherFactory->getPasswordHasher(User::class)->hash($userDetails['password']))
+            ;
+            $manager->persist($user);
+        }
+
+        $manager->flush();
+    }
+}
